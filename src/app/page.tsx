e224f47ee74dcaba9217d755b5b6ddd79eeddd50
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2024-03-02 14:07:07 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <header></header>
+      <main>
+        <h1>Trimmer</h1>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
