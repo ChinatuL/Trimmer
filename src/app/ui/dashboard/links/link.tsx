9f6c9d414df43547d38b77de98fb5e4bbb942e30
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2024-03-05 19:53:42 +0100

    feat: add link details modal

--- src/app/ui/dashboard/links/link.tsx
+++ src/app/ui/dashboard/links/link.tsx
@@ -1,27 +1,36 @@
-import { QRCodeSVG } from "qrcode.react";
+import QRCodeComponent from "@ui/qr-code";
 
 export default function LinkComponent({
+    id,
     longUrl,
     shortUrl,
     date,
     clicks,
+    handleLinkDetails,
 }: {
+    id: number;
     longUrl: string;
     shortUrl: string;
     date: string;
     clicks: number;
+    handleLinkDetails: (id: number) => void;
 }) {
     return (
-        <div className='w-full px-4 cursor-pointer'>
+        <div
+            className='w-full px-4 cursor-pointer'
+            onClick={() => handleLinkDetails(id)}
+        >
             <div className='grid grid-cols-[30%_25%_1fr_1fr_1fr] justify-center content-center items-center gap-4 border-b-2 border-b-[#262165] px-4 pt-4 pb-1'>
                 <div>
-                    <p className='truncate justify-self-start'>{longUrl}</p>
+                    <p className='truncate justify-self-start' id={longUrl}>
+                        {longUrl}
+                    </p>
                 </div>
                 <div>
-                    <p>{shortUrl}</p>
+                    <p id={shortUrl}>{shortUrl}</p>
                 </div>
                 <div className='justify-self-center'>
-                    <QRCodeSVG value={shortUrl} size={50} />
+                    <QRCodeComponent value={shortUrl} size={50} />
                 </div>
                 <div className='justify-self-center'>
                     <p>{date}</p>
